a7403f8dccde357a33cd1dfdaf756f11f20f7c78
masud1989 <mranasrj@gmail.com>
2023-11-18 21:54:04 +0600

    token-encode-decode-ok

--- src/app/api/token/route.js
+++ src/app/api/token/route.js
@@ -1,6 +1,7 @@
-import { SignJWT } from "jose"
+import { SignJWT, jwtVerify } from "jose"
 import { NextResponse } from "next/server"
 
+// Create/Encode Token 
 export async function GET(req, res){
     
     const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY)
@@ -14,6 +15,18 @@ export async function GET(req, res){
                     .setExpirationTime('5h')
                     .sign(secret)
 
-    return NextResponse.json({TokenData: token})
+    return NextResponse.json({token: token})
 
+}
+
+
+// Decode Token 
+export async function POST (req, res){
+    const JsonBody = await req.json()
+    const Token = JsonBody['token']
+
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET_KEY)
+    const Decoded = await jwtVerify(Token, secret)
+
+    return NextResponse.json(Decoded)
 }
